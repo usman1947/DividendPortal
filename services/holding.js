eb2fd047ca4882b055b6d1f0ef6551929f3fabe5
Usman <usman22aslam@gmail.com>
2023-04-02 15:16:51 +0800

    adding user id on save

--- services/holding.js
+++ services/holding.js
@@ -1,35 +1,40 @@
-const ClearbitLogo = require('clearbit-logo');
-let logo = new ClearbitLogo
+const ClearbitLogo = require("clearbit-logo");
+let logo = new ClearbitLogo();
 const express = require("express");
-const holdingModel = require("../models");
+const { HoldingModel } = require("../models");
 const app = express();
-const bp = require('body-parser')
-const {historical, quote} = require("./yahooFinance");
-const {calculateAverageAnnualGrowth} = require("../utils")
+const bp = require("body-parser");
+const { historical, quote } = require("./yahooFinance");
+const { calculateAverageAnnualGrowth } = require("../utils");
 
-const createHolding = async (holdings) => {
-    try {
-      const holdingDocs = await Promise.all(
-        holdings.map(async (holding) => {
-          const historicalDividendData = await historical(holding.ticker);
-          const data = await quote(holding.ticker);
-          const fiveYearCAGR = calculateAverageAnnualGrowth(historicalDividendData);
-          const companyInformation = await getLogo(data.displayName ?? data.shortName ?? data.longName)
-          return new holdingModel({
-            ...holding,
-            ...(companyInformation && companyInformation),
-            fiveYearCAGR,
-          });
-        })
-      );
-      return await holdingModel.insertMany(holdingDocs);
-    } catch (error) {
-      throw new Error(error);
-    }
+const createHolding = async (holdings, userId) => {
+  try {
+    const holdingDocs = await Promise.all(
+      holdings.map(async (holding) => {
+        const historicalDividendData = await historical(holding.ticker);
+        const data = await quote(holding.ticker);
+        const fiveYearCAGR = calculateAverageAnnualGrowth(
+          historicalDividendData
+        );
+        const companyInformation = await getLogo(
+          data.displayName ?? data.shortName ?? data.longName
+        );
+        return new HoldingModel({
+          ...holding,
+          ...(companyInformation && companyInformation),
+          fiveYearCAGR,
+          user_id: userId,
+        });
+      })
+    );
+    return await HoldingModel.insertMany(holdingDocs);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-async function getLogo(name){
-  return await logo.topSuggestion(name)
+async function getLogo(name) {
+  return await logo.topSuggestion(name);
 }
 
 exports.createHolding = createHolding;
